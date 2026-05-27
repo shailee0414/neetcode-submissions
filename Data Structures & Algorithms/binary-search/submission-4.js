621d446df73cfaacc0e8254dca51e3c2101d9421
shailee0414 <102052894+shailee0414@users.noreply.github.com>
2026-05-27 20:41:10 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0
+        let r = nums.length
+        let mid = -1
+        while(l<=r){
+            mid = Math.floor((l+r)/2)
+            if(nums[mid] == target) return mid
+            else if (target > nums[mid])l = mid + 1
+            else r = mid - 1
+        }
+        return -1
+    }
+}
